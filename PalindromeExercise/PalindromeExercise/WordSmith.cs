e194e7480892411dff8e71eb56867adc867afdbf
AJ Pilkerton <apilkerton3618@gmail.com>
2021-07-02 11:20:09 -0500

    added more test cases

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string input)
+        {
+            var str = input.Replace(" ", "");
+            if (str == "")
+            {
+                return false;
+            }
+            var strLow = str.ToLower();
+            var inputArr = strLow.ToCharArray();
+            Array.Reverse(inputArr);
+            var reverse = new string(inputArr);
+            if (strLow == reverse)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,30 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("", false)]
+        [InlineData("taco cat", true)]
+        [InlineData("1234321", true)]
+        [InlineData("43212345", false)]
+        [InlineData("lemons", false)]
+        [InlineData("Hello World", false)]
+        [InlineData("A new order began a more Roman age bred Rowena", true)]
 
+
+
+        public void IsAPalindrime(string input, bool expected)
+        {
+            var test = new WordSmith();
+
+            var actual = test.IsAPalindrome(input);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
